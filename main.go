e2035ec192c17366be4cee914df3848aa6e35f65
Paco <guopuke@gmail.com>
2018-07-08 04:40:19 +0800

    服务启动进行健康状态自检

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net/http"
 	"longo/router"
+	"log"
+	"time"
+	"errors"
 )
 
 func main() {
@@ -22,6 +25,28 @@ func main() {
 		middlewares...,
 	)
 
-	fmt.Printf("Start to lisenting the incoming requests on http address: %s", ":8080")
+	go func() {
+		if err := pingServer(); err != nil {
+			log.Fatal("The router has no response, or it might took too long to start up.\n", err)
+		}
+		log.Print("The router has been deployed successfully.")
+	}()
+
+	fmt.Printf("Start to lisenting the incoming requests on http address: %s\n", ":8080")
 	fmt.Printf(http.ListenAndServe(":8080", g).Error())
+}
+
+// pingServer pings the http server to make sure the router is working.
+// 服务启动进行健康状态自检
+func pingServer() error {
+	for i := 0; i < 2; i++ {
+		resp, e := http.Get("http://localhost:8080" + "/sd/health")
+		if e == nil && resp.StatusCode == http.StatusOK {
+			return nil
+		}
+
+		log.Print("Wating for the router, retry in 1 second.")
+		time.Sleep(time.Second)
+	}
+	return errors.New("Cannot connect to the router.\n")
 }
