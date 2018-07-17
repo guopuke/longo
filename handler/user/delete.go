3e6bc633194754d6ec5022757d6230b5123e45b6
Paco <guopuke@gmail.com>
2018-07-17 19:21:00 +0800

    调整删除失败的错误信息

--- handler/user/delete.go
+++ handler/user/delete.go
@@ -5,12 +5,13 @@ import (
 	. "github.com/guopuke/longo/handler"
 	"github.com/guopuke/longo/model"
 	"strconv"
+	"github.com/guopuke/longo/pkg/errno"
 )
 
 func Delete(c *gin.Context) {
 	userId, _ := strconv.Atoi(c.Param("id"))
 	if err := model.DeleteUser(uint64(userId)); err != nil {
-		SendResponse(c, err, nil)
+		SendResponse(c, errno.ErrDatabase, nil)
 		return
 	}
 	SendResponse(c, nil, nil)
